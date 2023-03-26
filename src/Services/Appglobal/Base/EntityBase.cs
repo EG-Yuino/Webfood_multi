a77975264eca364b1262bdcadf9b10db466df357
yuino <bombim209@gmail.com>
2023-03-26 14:08:00 +0700

    Code Base Appglobal

---
+++ src/Services/Appglobal/Base/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIglobal.Base
+{
+    public interface IEntityBase<TKey>
+    {
+        public TKey id { get; set; }
+    }
+
+    public abstract class EntityBase<TKey> : IEntityBase<TKey>
+    {
+        [Key]
+        public virtual TKey id { get; set; }
+    }
+}
